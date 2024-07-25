15112c14e14cd318cf4b73a0db2553dea54ff371
Kamila <kamila.tabishcheva@mail.ru>
2024-07-25 23:57:10 +0500

    Initial commit

---
+++ form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Авторизация и решистрация</title>
+  <link rel="stylesheet" href="main.css">
+</head>
+<body>
+         <!-- Форма авторизации -->
+
+        <form action="" method="">
+        <label>Логин</label>
+        <input type="text" placeholder="Введите свой логин" >
+        <label>Пароль</label>
+        <input type="password" placeholder="Потвердите свой пароль" >
+        <button>Войти</button>
+        <p>
+          У вас нет аккаунта? - <a href="/register.php">Зарегистрируйтесь</a>!
+        </p>
+        </form>
+
+</body>
+</html>
+
---
+++ register.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Авторизация и решистрация</title>
+  <link rel="stylesheet" href="main.css">
+</head>
+<body>
+         <!-- Форма авторизации -->
+
+        <form action="" method="" enctype="multipart/form-data">
+        <label>ФИО</label>
+        <input type="text" placeholder="Введите свое полное имя" >
+        <label>Логин</label>
+        <input type="text" placeholder="Введите свой логин" >
+        <label>Изображение профиля</label>
+        <input type="file" >
+        <label>Почта</label>
+        <input type="email" placeholder="Введите адрес своей почты" >
+        <label>Пароль</label>
+        <input type="password" placeholder="Введите свой пароль" >
+        <label>Потверждение пароля</label>
+        <input type="password" placeholder="Потвердите свой пароль" >
+        <button>Войти</button>
+        <p>
+          У вас уже есть аккаунт? - <a href="/form.php">Авторируйтесь</a>!
+        </p>
+        </form>
+
+</body>
+</html>
